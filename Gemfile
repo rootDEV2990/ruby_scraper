34a2d2a14e0cdd01baf64bdaa2df7215f6aea6de
rootDEV2990 <website.dev@icloud.com>
2020-08-10 07:40:20 -0500

    Finished scraper for scraper_torrents.rb

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 
 gem 'httparty'
 gem 'nokogiri'
+gem 'terminal-table'
 gem 'byebug'
---
+++ scraper_torrents.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'Nokogiri'
+require 'httparty'
+require 'terminal-table'
+require 'byebug'
+
+
+def scraper
+    url = "https://katcr.to/usearch/rick%20and%20morty/"
+    raw_data = HTTParty.get(url)
+    format_page = Nokogiri::HTML(raw_data)
+    #byebug
+    item_count = 0 
+    div = format_page.css('table.data')
+    table = div.css('tbody')
+    table.css('tr').each do |table_item| 
+
+
+        href = table_item.css('div.iaconbox a')
+        url = href.map {|link| link['href']}
+        
+        
+        title_extract = table_item.css('div.torrentname').text.to_s.split(/\n/)
+
+        torrent_info = []
+        title_extract.each {|text| torrent_info << text unless text.length == 0}
+        p item_count += 1
+        torrent_info.each_with_index {|title_item, index| p title_item unless index == 1 or index == 2 or index == 5}
+        p "https://katcr.to#{url[0]}"
+        p '***********************************************'
+    end
+
+    
+
+    #∫torrents = Array.new
+    #∫div.each do |li|
+        #∫torrents << torrent = { Title: li.css('span.item-title#reference a').text, Desc: li.css('span.item-icons#reference a').text }
+    #∫end
+    #∫p torrents[0...2]
+end
+
+scraper
+
