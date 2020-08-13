26024720369b32cf65d0ba98046843a6e9d42373
rootDEV2990 <website.dev@icloud.com>
2020-08-13 08:38:40 -0500

    Removed class from main file

--- bin/main.rb
+++ bin/main.rb
@@ -4,49 +4,45 @@ require 'httparty'
 require './lib/table.rb'
 require './lib/scraper.rb'
 
-class FetchTorrents < Scraper
-  attr_accessor :input, :page
+def app
+  p '/-/-/-/-/Search Torrents with Ruby and skip the pop-ups./-/-/-/-/'
+  p '/-/-/-/-/Whats the name of the torrent you need pirate?/-/-/-/-/'
+  input = gets.chomp
+  input = input.gsub(' ', '%20')
+  page = 1
+  display_results(page, input)
+end
 
-  def initialize
-    @input = gets.chomp
-    @input = @input.gsub(' ', '%20')
-    @page = 1
-    @crawling = true
-  end
-
-  def crawling_site
-    while @crawling == true
-      p 'To jump to a new page enter it bellow, or enter a new query. Type exit to exit the script.'
-      new_query = gets.downcase.chomp.to_s
-      if new_query == 'exit'
-        @crawling = false
-      elsif new_query.to_i.zero?
-        @input = new_query.to_s.gsub(' ', '%20')
-        @page = 1
-      elsif new_query.to_i > (@total / 20)
-        p 'Page does not exist, wait 3 seconds for reboot.'
-        sleep(3)
-      else
-        @page = new_query.to_i
-      end
-      display_results
+def crawling_site(total, input, page)
+  crawling = true
+  while crawling == true
+    p 'To jump to a new page enter it bellow, or enter a new query. Press Ctrl+C on your keyboard to exit.'
+    new_query = gets.downcase.chomp
+    if new_query.to_i.zero?
+      input = new_query.to_s.gsub(' ', '%20')
+      page = 1
+    elsif new_query.to_i > (total / 20)
+      p 'Page does not exist, wait 3 seconds for reboot.'
+      sleep(3)
+      page = page
+    else
+      page = new_query.to_i
     end
-  end
-
-  def display_results
-    scrape_site = Scraper.new(@input, @page)
-    puts scrape_site.extract_torrent
-    @total = scrape_site.total_results
-    table_object = ScrapedTable.new(@page, @input, @total)
-    table_object.display_table
-    crawling_site
+    display_results(page, input)
   end
 end
 
-p '/-/-/-/-/Search Torrents with Ruby and skip the pop-ups./-/-/-/-/'
-p '/-/-/-/-/Whats the name of the torrent you need pirate?/-/-/-/-/'
-app = FetchTorrents.new
-app.display_results
+def display_results(page, input)
+  scrape_site = Scraper.new(input, page)
+  puts scrape_site.extract_torrent
+  total = scrape_site.total_results
+  table_object = ScrapedTable.new(page, input, total)
+  table_object.display_table
+  crawling_site(total, input, page)
+end
+
+app
+
 p '/*/ Thank you for using Ruby Today /*/'
 p '/*/ Made with Ruby BC its AWESOME! /*/'
 p '/*/ Skip the Ads with your terminal/*/'
